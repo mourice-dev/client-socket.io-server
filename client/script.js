bd88a262f51ced562837e367ed5b333137783239
nshuti <nshutikope@gmail.com>
2026-06-24 13:30:32 +0200

    first fake chat box

--- client/script.js
+++ client/script.js
@@ -1,5 +1,7 @@
 /** @format */
 
+// const io = require("socket.io-client");
+
 import { io } from "socket.io-client";
 const sendMessage = document.querySelector("#send-message");
 const sendRoom = document.querySelector("#send-room");
@@ -8,12 +10,18 @@ const inputMessage = document.querySelector("#message");
 const form = document.querySelector("#form");
 
 const socket = io("http://localhost:3000");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = inputMessage.value;
   if (message === "") return;
   displayMessage(message);
+  socket.emit("message-send", message); // send message to server
   inputMessage.value = "";
+});
+
+socket.on("message-received", (message) => {
+  displayMessage(message); // receive message from server and display it
 });
 
 sendRoom.addEventListener("click", (e) => {
--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 /** @format */
 
-const io = require("socket.io")(3000, () => {
+const { Socket } = require("socket.io");
+
+const io = require("socket.io")(3000, {
   cors: {
-    origin: ["http://localhost:8080"];
-  }
+    origin: ["http://localhost:5173"],
+  },
 });
 // io.Server(sever());
 io.on("connection", (Socket) => {
   console.log(Socket.id);
-});
 
-io.on("data", (message) => {
-  console.log(message);
+  Socket.on("message-send", (message) => {
+    // listen message from any user
+    console.log(message);
+    Socket.broadcast.emit("message-received", message); // broadcast to every user
+  });
 });
